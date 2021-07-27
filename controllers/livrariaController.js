6f66121423aa6321296cde078964101dd58cfd3d
rafaelabonacim <rafaelabonacim@gmail.com>
2021-07-26 21:44:44 -0300

    api da livraria

---
+++ controllers/livrariaController.js
@@ -0,0 +1,49 @@
+const { Livros, Sequelize} = require('../models');
+
+
+const livrariaController = {
+    listar: async (req,res) => {
+        //listando professores com turmas relacionadas
+        const livros = await Livros.findAll({
+            order: [['titulo', 'ASC']]
+        });
+
+        return res.json(livros);
+    },
+    criar: async (req,res) =>{
+        const {titulo,quantidade_paginas,autor,ano_lancamento,estoque} = req.body;
+
+        const livros = await Livros.create({
+            titulo,
+            quantidade_paginas,
+            autor,
+            ano_lancamento,
+            estoque
+
+        });
+
+        return res.json(livros).status(200);
+    },
+    mostrar: async (req,res) =>{
+        const {id} = req.params;
+
+        const livro = await Livros.findByPk(id)
+
+        return res.json(livro).status(200);
+    },
+    atualizar: async (req,res) => {
+        const {id} = req.params;
+        const {titulo,quantidade_paginas,autor,ano_lancamento,estoque} = req.body;
+
+        const livro = await Livros.update({
+            titulo,
+            quantidade_paginas,
+            autor,
+            ano_lancamento,
+            estoque
+        }, {where: {id}})
+        return res.json(livro).status(200);
+    }
+}
+    
+module.exports = livrariaController;
